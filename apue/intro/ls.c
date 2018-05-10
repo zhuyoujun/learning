61cc069a46a565d3c63fa8e52eec5b28c20c64c8
zhuyoujun <zhuyoujun@users.noreply.github.com>
2018-05-10 20:43:46 +0800

    update

---
+++ apue/intro/ls.c
@@ -0,0 +1,12 @@
+#include<dirent.h>
+
+int main(int argc, char *argv[])
+{
+    DIR *dp;
+    struct dirent *dirp;
+    if(argc != 2)
+    {
+        err_quit("usage:ls directory_name");
+    }
+
+}
